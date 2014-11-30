0b8e74ff12bf35aca17f1c94a3f1ab7a7028546c
Ben Nuttall <ben@bennuttall.com>
2014-11-30 17:27:58 +0000

    Add printing temperature to lcd

--- chef-hat/chef-hat.py
+++ chef-hat/chef-hat.py
@@ -2,19 +2,26 @@ from energenie import switch_on, switch_off
 from time import sleep
 import RPi.GPIO as GPIO
 from w1thermsensor import W1ThermSensor
+import lcd
 
 desired_temp = 21
 
 
 def main():
     sensor = W1ThermSensor()
+    lcd.init()
     while True:
         temp = sensor.get_temperature()
         print(temp)
+        lcd.write("%s" % temp)
         if temp < desired_temp:
             switch_on()
+            print("on")
+            lcd.write("on", 2)
         else:
             switch_off()
+            print("off")
+            lcd.write("off", 2)
         sleep(5)
 
 if __name__ == '__main__':
