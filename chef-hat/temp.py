fc77b73ccb1c170f314fd71fd87e4143a7c8859f
Ben Nuttall <ben@bennuttall.com>
2014-11-30 10:36:56 +0000

    Check device set up before checking temperature

--- chef-hat/temp.py
+++ chef-hat/temp.py
@@ -1,17 +1,25 @@
 from glob import glob
+import os
 from time import sleep
 
-device_location = '/sys/bus/w1/devices/28*'
-device_dir = glob(device_location)[0]
-device_file = device_dir + '/w1_slave'
-
+def get_device_file():
+    devices_path = '/sys/bus/w1/devices/'
+    devices = glob(devices_path + '28*')
+    if not devices:
+        os.system('sudo modprobe w1-gpio')
+        os.system('sudo modprobe w1-therm')
+    devices = glob(devices_path + '28*')
+    device_file = devices[0] + '/w1_slave'
+    return device_file
 
 def take_temp():
-    with open(device_file) as f:
-        text = f.read()
+    with open(device_file) as df:
+        text = df.read()
     temp_data = text.split('\n')[1].split()[9]
     temp = float(temp_data[2:]) / 1000
     return temp
+
+device_file = get_device_file()
 
 def main():
     while True:
