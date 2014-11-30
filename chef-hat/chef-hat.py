97461f41abf9fadbc583ede268330e3695e3c694
Ben Nuttall <ben@bennuttall.com>
2014-11-30 12:40:24 +0000

    Use w1thermsensor library

--- chef-hat/chef-hat.py
+++ chef-hat/chef-hat.py
@@ -1,14 +1,15 @@
 from energenie import switch_on, switch_off
 from time import sleep
 import RPi.GPIO as GPIO
-from temp import take_temp
+from w1thermsensor import W1ThermSensor
 
 desired_temp = 21
 
 
 def main():
+    sensor = W1ThermSensor()
     while True:
-        temp = take_temp()
+        temp = sensor.get_temperature()
         print(temp)
         if temp < desired_temp:
             switch_on()
