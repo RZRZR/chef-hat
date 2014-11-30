69658cd7a89f0ad0f072e5df0322a5cb37c79e62
Ben Nuttall <ben@bennuttall.com>
2014-11-30 10:37:59 +0000

    Test script for using energenie with temperature sensor

--- chef-hat/chef-hat.py
+++ chef-hat/chef-hat.py
@@ -1,21 +1,19 @@
 from energenie import switch_on, switch_off
-import lcd
 from time import sleep
 import RPi.GPIO as GPIO
+from temp import take_temp
+
+desired_temp = 21
 
 def main():
-    lcd.init()
-    lcd.write("Sous", 1)
-    lcd.write("Vide", 2)
-    sleep(2)
-    lcd.write("Turning", 1)
-    lcd.write("on", 2)
-    sleep(2)
-    switch_on(0)
-    lcd.write("Turning", 1)
-    lcd.write("off", 2)
-    sleep(2)
-    switch_off(0)
+    while True:
+        temp = take_temp()
+        print(temp)
+        if temp > desired_temp:
+            switch_on()
+        else:
+            switch_off()
+        sleep(5)
 
 if __name__ == '__main__':
     try:
